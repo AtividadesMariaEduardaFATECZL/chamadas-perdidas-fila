9b4a8f1eaf0da40dfdf5459f52a82e8abd8077be
mariasantosdev <mariaeduardasrodolfo2@gmail.com>
2021-11-15 21:02:49 -0300

    first commit

---
+++ src/controller/TelefoneController.java
@@ -0,0 +1,30 @@
+package controller;
+
+import com.maria.utils.fila.string.Fila;
+
+import javax.swing.*;
+
+public class TelefoneController {
+
+	public TelefoneController() {
+		super();
+	}
+
+	public void insereLigacao(Fila f, String numeroTelefone) {
+		f.insert(numeroTelefone);
+	}
+
+	public void consultaLigacoes(Fila f) {
+		if (f.isEmpty()) {
+			JOptionPane.showMessageDialog(null, "N�o h� chamadas perdidas");
+		} else {
+			try {
+				String chamada = f.remove();
+				JOptionPane.showMessageDialog(null, chamada);
+			} catch (Exception e) {
+				JOptionPane.showMessageDialog(null, "N�o h� chamadas perdidas");
+			}
+		}
+	}
+
+}
---
+++ src/view/Principal.java
@@ -0,0 +1,36 @@
+package view;
+
+import com.maria.utils.fila.string.Fila;
+import controller.TelefoneController;
+
+import javax.swing.*;
+
+public class Principal {
+
+	public static void main(String[] args) {
+		Fila f = new Fila();
+		TelefoneController telefone = new TelefoneController();
+		
+		String[] actionButtons = { "Adicionar chamada", "Consultar chamadas perdidas", "Cancelar" };
+		
+		int opcao = 0;
+
+		do {
+			opcao = JOptionPane.showOptionDialog(null, "Qual ser� a a��o?", "Confirmation",
+					JOptionPane.INFORMATION_MESSAGE, 0, null, actionButtons, actionButtons[0]);
+			
+			switch (opcao) {
+				case 0:
+					String chamada = JOptionPane.showInputDialog(null, "Insira um n�mero de telefone");
+					telefone.insereLigacao(f, chamada);
+					break;
+				case 1:
+					telefone.consultaLigacoes(f);
+					break;
+			}
+
+		} while (opcao != 2);
+		
+	}
+
+}
